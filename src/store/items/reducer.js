8de4042e7e06f2b7f1a2df6428bda23a0df5cb81
Paavo <paavo.nelimarkka@gmail.com>
2022-01-20 10:28:41 +0200

    add update price & quantity, add summary

--- src/store/items/reducer.js
+++ src/store/items/reducer.js
@@ -1,4 +1,4 @@
-import { ITEM_ADDED, ITEM_REMOVED } from './actions'
+import { ITEM_ADDED, ITEM_PRICE_UPDATED, ITEM_QUANTITY_UPDATED, ITEM_REMOVED } from './actions'
 let id = 1;
 
 export const initialItems = [
@@ -7,12 +7,32 @@ export const initialItems = [
 ];
 
 export const reducer = (state = initialItems, action) => {
+  // ITEM ADDED
   if (action.type === ITEM_ADDED) {
     const item = { uuid: id++, quantity: 1, ...action.payload }
     return [...state, item]
   }
+  // ITEM REMOVED
   if (action.type === ITEM_REMOVED) {
-    return state.filter(item => item.uuid != action.payload.uuid)
+    return state.filter(item => item.uuid !== action.payload.uuid)
+  }
+  // ITEM PRICE UPDATED
+  if (action.type === ITEM_PRICE_UPDATED) {
+    return state.map(item => {
+      if (item.uuid === action.payload.uuid) {
+        return { ...item, price: action.payload.price }
+      }
+      return item
+    })
+  }
+  // ITEM QUANTITY UPDATED
+  if (action.type === ITEM_QUANTITY_UPDATED) {
+    return state.map(item => {
+      if (item.uuid === action.payload.uuid) {
+        return { ...item, quantity: action.payload.quantity }
+      }
+      return item
+    })
   }
 
   return state;
